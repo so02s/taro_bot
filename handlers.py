3d2369e061d87278417159e29f62adfbda1cc66c
so02s <so02s@mail.ru>
2025-04-03 11:12:29 +0300

    first ver, no AI

---
+++ handlers.py
@@ -0,0 +1,23 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.types.input_file import FSInputFile
+from cards import cards
+
+router = Router()
+
+@router.message(F.text.capitalize().in_(cards.keys()))
+async def echo_card(msg: Message):
+    key = msg.text.capitalize()
+    photo = FSInputFile(f"./img/{key}.png")
+    await msg.answer_photo(
+        caption=f"*{key}*\n" + cards[key],
+        parse_mode='Markdown',
+        photo=photo
+    )
+
+# TODO При вводе карты дать возможность задать вопрос (через FSM + redis)
+# Вопрос и заготовка отправляется на API GigaChat -> Дается ответ из нейронки + Меченная ссылка
+
+@router.message()
+async def error(msg: Message):
+    await msg.answer("Кажется, вы ввели что-то не то.\nПожалуйста, попробуйте ещё раз!")
---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from decouple import config
+from handlers import router
+
+bot = Bot(token=config("BOT_TOKEN"))
+
+async def main():
+    dp = Dispatcher()
+    dp.include_router(router)
+    try:
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
